54d97e57a7f44a1da3ec526e97cb2f8772a0e6d8
Abhi Asokan <aa@bolikka.com>
2019-11-12 13:17:38 +1100

    Added input check

--- src/api.js
+++ src/api.js
@@ -15,23 +15,48 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.json({foo: 'bar'});
 });
-
+function isValidInput(count, instruction) {
+    let regex = /^[<>v\^x]*$/;
+    if(count == undefined || instruction == undefined){
+        return false;
+    }
+    if (!instruction.match(regex)){
+        return false
+    }
+    if(Number.isNaN(count)){
+        return false;
+    }
+    if((!Number.isInteger(Number(count)))||(count<0)){
+        return false
+    }
+    return true;
+};
 //process drone instruction requests
 app.post('/drone', function(req, res) {
     console.log(req.body);
-    var count = req.body.droneCount;
-    var instructions = req.body.droneInstructions;
-    drones = [];
-    billboards = [];
-    for (let i = 0; i < count; i++) {
-        drones.push(new Drone(instructions.match(new RegExp('.{1,'+count+'}','g')).map(x=>x[i]).filter(x=>x!=null)));
+    var count;
+    var instructions;
+
+    count = req.body.droneCount;
+    instructions = req.body.droneInstructions;
+    if(!isValidInput(count,instructions)){
+        res.statusCode = 400;
+        res.send("invalid input");
+    }else{    
+        console.log('instructoins: ',instructions)
+        drones = [];
+        billboards = [];
+        for (let i = 0; i < count; i++) {
+            drones.push(new Drone(instructions.match(new RegExp('.{1,'+count+'}','g')).map(x=>x[i]).filter(x=>x!=null)));
+        }
+        drones.forEach(drone=>{
+            billboards.push(...drone.getBillboards());
+        });
+        console.log(billboards);
+        res.json({no_of_billboards: _.uniqWith(billboards,_.isEqual).length});
     }
-    drones.forEach(drone=>{
-        billboards.push(...drone.getBillboards());
-    });
-    console.log(billboards);
-    res.json({no_of_billboards: _.uniqWith(billboards,_.isEqual).length});
 });
+
 
 app.listen(4001, () => console.log(`Api started at http://localhost:4001`));
 
