8e938247e6fc672166d1657622a326ea4a8d9710
chang <chang@goodhuman.me>
2019-11-13 17:22:12 +1100

    -- finished with features and low-fi UI

--- src/api.js
+++ src/api.js
@@ -4,11 +4,183 @@ const cors = require('cors');
 const path = require('path');
 
 
+function process(str){
+  var ar = [];
+  var current = {x: 0, y: 0};
+  var length = str.length;
+  for(let i = 0; i < str.length; i++){
+    switch(str[i]){
+      case 'x':
+        ar.push('x'+current.x+'y'+current.y);
+        // console.log('photo taken');
+        break;
+      case '^':
+        current = {...current, y: current.y+1};
+        break;
+      case 'v':
+        current = {...current, y: current.y-1};
+        break;
+      case '<':
+        current = {...current, x: current.x-1};
+        break;
+      case '>':
+        current = {...current, x: current.x+1};
+        break;
+      default:
+        console.log('???');
+    }
+  }
+  console.log(ar.length);
+  console.log(Array.from(new Set(ar)).length);
+}
+
+function process2(str){
+  var ar = [];
+  var current1 = {x: 0, y: 0};
+  var current2 = {x: 0, y: 0};
+  var length = str.length;
+  for(let i = 0; i < str.length; i++){
+    if(i%2 === 0){
+      switch(str[i]){
+        case 'x':
+          ar.push('x'+current1.x+'y'+current1.y);
+          // console.log('photo taken');
+          break;
+        case '^':
+          current1 = {...current1, y: current1.y+1};
+          break;
+        case 'v':
+          current1 = {...current1, y: current1.y-1};
+          break;
+        case '<':
+          current1 = {...current1, x: current1.x-1};
+          break;
+        case '>':
+          current1 = {...current1, x: current1.x+1};
+          break;
+        default:
+          console.log('???');
+      }
+    }
+    else{
+      switch(str[i]){
+        case 'x':
+          ar.push('x'+current2.x+'y'+current2.y);
+          // console.log('photo taken');
+          break;
+        case '^':
+          current2 = {...current2, y: current2.y+1};
+          break;
+        case 'v':
+          current2 = {...current2, y: current2.y-1};
+          break;
+        case '<':
+          current2 = {...current2, x: current2.x-1};
+          break;
+        case '>':
+          current2 = {...current2, x: current2.x+1};
+          break;
+        default:
+          console.log('???');
+      }
+    }
+  }
+  console.log(ar.length);
+  return (Array.from(new Set(ar)).length);
+}
+
+
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.json({foo: 'bar'});
+    // console.log('Request: ', req.query);
+    res.json({data: req.query.token});
 });
 
-app.listen(4001, () => console.log(`Api started at http://localhost:4001`));
+app.get('/getBillBoards1/', (req, res) => {
+  let str = req.query.token;
+  var ar = [];
+  var current = {x: 0, y: 0};
+  var length = str.length;
+  for(let i = 0; i < str.length; i++){
+    switch(str[i]){
+      case 'x':
+        ar.push('x'+current.x+'y'+current.y);
+        // console.log('photo taken');
+        break;
+      case '^':
+        current = {...current, y: current.y+1};
+        break;
+      case 'v':
+        current = {...current, y: current.y-1};
+        break;
+      case '<':
+        current = {...current, x: current.x-1};
+        break;
+      case '>':
+        current = {...current, x: current.x+1};
+        break;
+      default:
+        console.log('???');
+    }
+  }
+  res.json({data: Array.from(new Set(ar)).length});
+});
+//
+app.get('/getBillBoards2/', (req, res) => {
+  let str = req.query.token;
+  var ar = [];
+  var current1 = {x: 0, y: 0};
+  var current2 = {x: 0, y: 0};
+  var length = str.length;
+  for(let i = 0; i < str.length; i++){
+    if(i%2 === 0){
+      switch(str[i]){
+        case 'x':
+          ar.push('x'+current1.x+'y'+current1.y);
+          // console.log('photo taken');
+          break;
+        case '^':
+          current1 = {...current1, y: current1.y+1};
+          break;
+        case 'v':
+          current1 = {...current1, y: current1.y-1};
+          break;
+        case '<':
+          current1 = {...current1, x: current1.x-1};
+          break;
+        case '>':
+          current1 = {...current1, x: current1.x+1};
+          break;
+        default:
+          console.log('???');
+      }
+    }
+    else{
+      switch(str[i]){
+        case 'x':
+          ar.push('x'+current2.x+'y'+current2.y);
+          // console.log('photo taken');
+          break;
+        case '^':
+          current2 = {...current2, y: current2.y+1};
+          break;
+        case 'v':
+          current2 = {...current2, y: current2.y-1};
+          break;
+        case '<':
+          current2 = {...current2, x: current2.x-1};
+          break;
+        case '>':
+          current2 = {...current2, x: current2.x+1};
+          break;
+        default:
+          console.log('???');
+      }
+    }
+  }
+  res.json({data: Array.from(new Set(ar)).length});
+});
 
+
+app.listen(4001, () => console.log(`Api started at http://localhost:4001`));
--- src/client.js
+++ src/client.js
@@ -1,15 +1,95 @@
-import React from 'react';
+import React, { useEffect, useState, Component } from 'react';
+import axios from 'axios';
 import ReactDOM from 'react-dom';
 import useFetch from 'react-fetch-hook';
 
 
-function App() {
-    const {isLoading, data} = useFetch('http://localhost:4001');
-    if(isLoading) {
-        return 'Loading...';
-    }
-    return <pre>{JSON.stringify(data, null, 4)}</pre>;
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { token: '', billBoards: 0 }
+  }
+  async componentDidMount(){
+    // const result = await axios.get(
+    //   'http://localhost:4001/getBillBoards2',
+    //   {params: { token: 'x^xv' }}
+    // );
+    // this.setState({ billBoards: result.data.data });
+  }
+  async getBillBoards1(){
+    const result = await axios.get(
+      'http://localhost:4001/getBillBoards1',
+      {params: { token: this.state.token }}
+    );
+    this.setState({ billBoards: result.data.data });
+  }
+  async getBillBoards2(){
+    const result = await axios.get(
+      'http://localhost:4001/getBillBoards2',
+      {params: { token: this.state.token }}
+    );
+    this.setState({ billBoards: result.data.data });
+  }
+  handleChange(event){
+    this.setState({ token: event.target.value });
+  }
+  render(){
+    return(
+      <div>
+        <textarea name="body"
+          onChange={(event)=>{
+            this.handleChange(event);
+          }}
+          value={this.state.token}
+        />
+        <button onClick={async() => {
+          await this.getBillBoards1();
+        }}>
+          Get BillBoards(Part1)
+        </button>
+        <button onClick={async() => {
+          await this.getBillBoards2();
+        }}>
+          Get BillBoards(Part2)
+        </button>
+        <p>BillBoards Count: {this.state.billBoards}</p>
+      </div>
+    )
+  }
 }
+
+// function App() {
+//   const [data, setData] = useState({ hits: 0 });
+    // useEffect(() => {
+    //   // async function fetchData() {
+    //   //   const result = await axios(
+    //   //     'http://localhost:4001',
+    //   //   );
+    //   //   console.log('Data: ', result.data.data);
+    //   //   setData(2);
+    //   // }
+    //   // fetchData();
+    //   setData(2);
+    // }, []);
+    // setData(2);
+
+//   const [count, setCount] = useState(0);
+//
+// // Similar to componentDidMount and componentDidUpdate:
+// useEffect(() => {
+//   // Update the document title using the browser API
+//   document.title = `You clicked ${count} times`;
+// });
+//
+// return (
+//   <div>
+//     <p>You clicked {count} times</p>
+//     <button onClick={() => setCount(count + 1)}>
+//       Click me
+//     </button>
+//   </div>
+// );
+// }
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
