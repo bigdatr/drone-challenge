ec1107ee5373ff8841cc836d40dbfffbdbf0dad3
melvey <matthew.elvey@cdu.edu.au>
2020-11-03 18:00:15 +0930

    Handle long paths in results display

--- src/client/components/results/results.jsx
+++ src/client/components/results/results.jsx
@@ -5,14 +5,15 @@ import DroneGraphic from '../drone_graphic/drone_graphic.jsx';
 
 function DroneResults(props) {
 	const {path, photos, unique, drones, numPhotos} = props.results;
+	console.log('style', style);
 
 	return (<div className={style.results}>
 		<h2>Drone Results</h2>
 		<ul>
-			<li>{`Path: ${path}`}</li>
 			<li>{`Total Photographs: ${numPhotos}`}</li>
 			<li>{`Unique Photographs: ${unique}`}</li>
 			<li>{`Number of drones: ${drones}`}</li>
+			<li className={style.path} ><span>Path: </span><span className={style.pathField}>{path}</span></li>
 		</ul>
 		<h3>Photo locations</h3>
 		<DroneGraphic photos={photos} drones={drones} />
